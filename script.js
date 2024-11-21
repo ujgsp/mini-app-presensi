cd5a8f8c73f214edbd97e3ba3cd0add662f9fa09
ujgsp <ujangsp11@gmail.com>
2024-11-21 16:21:52 +0700

    Refactor: script

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 function checkIn() {
-<<<<<<< HEAD
     fetch('https://script.google.com/macros/s/AKfycbwQshPrA768k6u-a-ElFo8O6dbQjcUqWdQu43XaSRV0LTULcTjuPuJ7F6E8o-qnDYoM/exec', {
             method: 'POST',
-            // mode: 'no-cors',
+            mode: 'no-cors',
             headers: {
-                'Content-Type': 'text/plain;charset=utf-8'
+                'Content-Type': 'application/json'
             },
             body: JSON.stringify({
                 action: 'checkin'
@@ -24,24 +23,8 @@ function checkIn() {
             setAlert += '</div>';
             $('#status').html(setAlert);
         });
-=======
-    fetch('https://script.google.com/macros/s/AKfycbwQshPrA768k6u-a-ElFo8O6dbQjcUqWdQu43XaSRV0LTULcTjuPuJ7F6E8o-qnDYoM/exec?action=checkin', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          'Origin': 'https://ujgsp.github.io'
-        },
-        body: JSON.stringify({
-          action: 'checkin'
-        }),
-        mode: 'cors'  // Pastikan Anda menambahkan mode: 'cors'
-      })
-      .then(response => response.json())
-      .then(data => console.log(data))
-      .catch(error => console.error('Error:', error));
-      
->>>>>>> 5cf689b65ac17dfc9ad89fda26e17e99d9c856fb
 }
+
 
 
 function checkOut() {
