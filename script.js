f8cc5e810d3893289af9222125675e6a6e65b0f9
ujgsp <ujangsp11@gmail.com>
2024-11-21 13:22:12 +0700

    Perbaikan code

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 function checkIn() {
-    fetch('/checkin', { method: 'POST' })
+    fetch('https://script.google.com/macros/s/AKfycbwQshPrA768k6u-a-ElFo8O6dbQjcUqWdQu43XaSRV0LTULcTjuPuJ7F6E8o-qnDYoM/exec?action=checkin', { method: 'POST' })
         .then(response => response.json())
         .then(data => alert(data.message))
         .catch(error => alert('Gagal Check In.'));
 }
 
 function checkOut() {
-    fetch('/checkout', { method: 'POST' })
+    fetch('https://script.google.com/macros/s/AKfycbwQshPrA768k6u-a-ElFo8O6dbQjcUqWdQu43XaSRV0LTULcTjuPuJ7F6E8o-qnDYoM/exec?action=checkout', { method: 'POST' })
         .then(response => response.json())
         .then(data => alert(data.message))
         .catch(error => alert('Gagal Check Out.'));
