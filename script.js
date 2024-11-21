96b54fa7cd97c651ddfe7a7b2b74273c10ca844f
ujgsp <ujangsp11@gmail.com>
2024-11-21 15:43:41 +0700

    refactor: script.js

--- script.js
+++ script.js
@@ -1,13 +1,40 @@
 function checkIn() {
-    fetch('https://script.google.com/macros/s/AKfycbwQshPrA768k6u-a-ElFo8O6dbQjcUqWdQu43XaSRV0LTULcTjuPuJ7F6E8o-qnDYoM/exec?action=checkin', { method: 'POST' })
-        .then(response => response.json())
-        .then(data => alert(data.message))
-        .catch(error => alert('Gagal Check In.'));
+    fetch('https://script.google.com/macros/s/AKfycbwQshPrA768k6u-a-ElFo8O6dbQjcUqWdQu43XaSRV0LTULcTjuPuJ7F6E8o-qnDYoM/exec', {
+            method: 'POST',
+            mode: 'no-cors',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                action: 'checkin'
+            })
+        })
+        .then(response => {
+            if (!response.ok) throw new Error('Network response was not ok');
+            return response.json();
+        })
+        .then(data => {
+            // console.log(data); // Tampilkan data yang diterima
+        })
+        .catch(error => {
+            let setAlert = '';
+            setAlert += '<div class="alert alert-danger" role="alert">';
+            setAlert += 'Error: ' + error;
+            setAlert += '</div>';
+            $('#status').html(setAlert);
+        });
 }
 
+
 function checkOut() {
-    fetch('https://script.google.com/macros/s/AKfycbwQshPrA768k6u-a-ElFo8O6dbQjcUqWdQu43XaSRV0LTULcTjuPuJ7F6E8o-qnDYoM/exec?action=checkout', { method: 'POST' })
-        .then(response => response.json())
-        .then(data => alert(data.message))
-        .catch(error => alert('Gagal Check Out.'));
+    $.ajax({
+        url: 'https://script.google.com/macros/s/AKfycbwQshPrA768k6u-a-ElFo8O6dbQjcUqWdQu43XaSRV0LTULcTjuPuJ7F6E8o-qnDYoM/exec',
+        method: 'POST',
+        success: function(data) {
+            alert(data.message); // Tampilkan pesan dari response
+        },
+        error: function(xhr, status, error) {
+            alert('Gagal Check Out.'); // Tangani error jika gagal
+        }
+    });
 }
