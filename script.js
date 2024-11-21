5cf689b65ac17dfc9ad89fda26e17e99d9c856fb
Ujang Sopiyan <40892039+ujgsp@users.noreply.github.com>
2024-11-21 13:56:02 +0700

    refactor: checkin

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
 function checkIn() {
-    fetch('https://script.google.com/macros/s/AKfycbwQshPrA768k6u-a-ElFo8O6dbQjcUqWdQu43XaSRV0LTULcTjuPuJ7F6E8o-qnDYoM/exec?action=checkin', { method: 'POST' })
-        .then(response => response.json())
-        .then(data => alert(data.message))
-        .catch(error => alert('Gagal Check In.'));
+    fetch('https://script.google.com/macros/s/AKfycbwQshPrA768k6u-a-ElFo8O6dbQjcUqWdQu43XaSRV0LTULcTjuPuJ7F6E8o-qnDYoM/exec?action=checkin', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Origin': 'https://ujgsp.github.io'
+        },
+        body: JSON.stringify({
+          action: 'checkin'
+        }),
+        mode: 'cors'  // Pastikan Anda menambahkan mode: 'cors'
+      })
+      .then(response => response.json())
+      .then(data => console.log(data))
+      .catch(error => console.error('Error:', error));
+      
 }
 
 function checkOut() {
